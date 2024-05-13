3ad4d659bbfba1f665055ffdffc4970d82a13981
erayfirat <firateray@gmail.com>
2024-05-13 21:01:45 +0200

    first commit 1-7

---
+++ SuggestionAppUI/RegisterServices.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SuggestionAppUI;
+
+public static class RegisterServices
+{
+    public static void ConfigurationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddMemoryCache();
+    }
+}
