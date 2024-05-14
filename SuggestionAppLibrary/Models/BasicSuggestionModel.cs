aeac767189f6bc86054608ce3d4bba1ee83fe3fc
erayfirat <firateray@gmail.com>
2024-05-14 09:37:38 +0200

    gitignore file and lesson 8

---
+++ SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicSuggestionModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Suggestion { get; set; }
+    public BasicSuggestionModel()
+    {
+
+    }
+    public BasicSuggestionModel(SuggestionModel suggestion)
+    {
+        Id = suggestion.Id;
+        Suggestion = suggestion.Suggestion;
+    }
+}
+
